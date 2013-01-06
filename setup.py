8c69f2b1d0cc86c0e4c3807176dbe628f637eb18
Wieland Hoffmann <themineo@gmail.com>
2013-01-06 21:05:01 +0100

    squashed history

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from distutils.core import setup
+
+setup(name="mplayer-autocmd",
+        version="1",
+        author="Wieland Hoffmann",
+        author_email="themineo@gmail.com",
+        scripts=["automp"],
+        download_url=["https://github.com/mineo/mplayer-autocmd/tarball/master"],
+        url=["http://github.com/mineo/mplayer-autocmd"],
+        license="MIT",
+        classifiers=[
+            "Environment :: Console",
+            "License :: OSI Approved :: MIT License",
+            "Natural Language :: English",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python :: 3.3"],
+        data_files=[("share/zsh/site-functions", ["_automp"])],
+        )
