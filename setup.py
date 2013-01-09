23cde9c6a2ea7970d913daa16736326f6385eda6
Wieland Hoffmann <themineo@gmail.com>
2013-01-09 19:13:01 +0100

    update setup.py with descriptions

--- setup.py
+++ setup.py
@@ -2,18 +2,20 @@
 from distutils.core import setup
 
 setup(name="mplayer-autocmd",
-        version="1",
-        author="Wieland Hoffmann",
-        author_email="themineo@gmail.com",
-        scripts=["automp"],
-        download_url=["https://github.com/mineo/mplayer-autocmd/tarball/master"],
-        url=["http://github.com/mineo/mplayer-autocmd"],
-        license="MIT",
-        classifiers=[
-            "Environment :: Console",
-            "License :: OSI Approved :: MIT License",
-            "Natural Language :: English",
-            "Operating System :: OS Independent",
-            "Programming Language :: Python :: 3.3"],
-        data_files=[("share/zsh/site-functions", ["_automp"])],
-        )
+      version="1",
+      author="Wieland Hoffmann",
+      author_email="themineo@gmail.com",
+      description="mplayer wrapper providing per-file options & keybinding",
+      long_description=open("README.rst").read(),
+      scripts=["automp"],
+      download_url=["https://github.com/mineo/mplayer-autocmd/tarball/master"],
+      url=["http://github.com/mineo/mplayer-autocmd"],
+      license="MIT",
+      classifiers=[
+      "Environment :: Console",
+      "License :: OSI Approved :: MIT License",
+      "Natural Language :: English",
+      "Operating System :: OS Independent",
+      "Programming Language :: Python :: 3.3"],
+      data_files=[("share/zsh/site-functions", ["_automp"])],
+      )
